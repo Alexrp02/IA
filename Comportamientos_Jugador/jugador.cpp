f6eaeb1d16555f1c8f7cc3773b611486065b0025
AlexRP <aleeexrp02@gmail.com>
2023-03-15 23:56:16 +0100

    Added moving functionality

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -2,30 +2,123 @@
 #include <iostream>
 using namespace std;
 
-
-
-Action ComportamientoJugador::think(Sensores sensores){
-
+Action ComportamientoJugador::think(Sensores sensores)
+{
 	Action accion = actIDLE;
+	int a;
+	switch (last_action)
+	{
+	case actFORWARD:
+		// Actualización en caso de avanzar
+		switch (current_state.brujula)
+		{
+		case norte:
+			current_state.fil--;
+			break;
+		case noreste:
+			current_state.fil--;
+			current_state.col++;
+			break;
+		case este:
+			current_state.col++;
+			break;
+		case sureste: /*Actualizacion*/
+			current_state.fil++;
+			current_state.col++;
+			break;
+		case sur: /*Actualizacion*/
+			current_state.fil++;
+			break;
+		case suroeste: /*Actualizacion*/
+			current_state.fil++;
+			current_state.col--;
+			break;
+		case oeste: /*Actualizacion*/
+			current_state.col--;
+			break;
+		case noroeste: /*Actualizacion*/
+			current_state.fil--;
+			current_state.col--;
+			break;
+		}
+		break;
+	case actTURN_SL:
+		a = current_state.brujula;
+		a = (a + 7) % 8;
+		girar_derecha = (rand() % 2 == 0);
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	case actTURN_SR:
+		a = current_state.brujula;
+		a = (a + 1) % 8;
+		girar_derecha = (rand() % 2 == 0);
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	case actTURN_BL:
+		// Actualización de girar 135º a la izquierda
+		a = current_state.brujula;
+		a = (a + 5) % 8;
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	case actTURN_BR:
+		// Actualización en caso de girar 135º a la derecha
+		a = current_state.brujula;
+		a = (a + 3) % 8;
+		current_state.brujula = static_cast<Orientacion>(a);
+		break;
+	}
+
+	if ((sensores.terreno[2] =='T' || sensores.terreno[2] =='S') &&
+		sensores.superficie[2] =='_')
+	{
+		accion = actFORWARD;
+	}
+	else if (!girar_derecha)
+	{
+		accion = actTURN_SL;
+		girar_derecha = (rand() % 2 == 0);
+	}
+	else
+	{
+		accion = actTURN_SR;
+		girar_derecha = (rand() % 2 == 0);
+	}
 
 	cout << "Posicion: fila " << sensores.posF << " columna " << sensores.posC << " ";
-	switch(sensores.sentido){
-		case 0: cout << "Norte" << endl; break;
-		case 1: cout << "Noreste" << endl; break;
-		case 2: cout << "Este" << endl; break;
-		case 3: cout << "Sureste" << endl; break;
-		case 4: cout << "Sur " << endl; break;
-		case 5: cout << "Suroeste" << endl; break;
-		case 6: cout << "Oeste" << endl; break;
-		case 7: cout << "Noroeste" << endl; break;
+	switch (sensores.sentido)
+	{
+	case 0:
+		cout << "Norte" << endl;
+		break;
+	case 1:
+		cout << "Noreste" << endl;
+		break;
+	case 2:
+		cout << "Este" << endl;
+		break;
+	case 3:
+		cout << "Sureste" << endl;
+		break;
+	case 4:
+		cout << "Sur " << endl;
+		break;
+	case 5:
+		cout << "Suroeste" << endl;
+		break;
+	case 6:
+		cout << "Oeste" << endl;
+		break;
+	case 7:
+		cout << "Noroeste" << endl;
+		break;
 	}
 	cout << "Terreno: ";
-	for (int i=0; i<sensores.terreno.size(); i++)
+	for (int i = 0; i < sensores.terreno.size(); i++)
 		cout << sensores.terreno[i];
 	cout << endl;
 
 	cout << "Superficie: ";
-	for (int i=0; i<sensores.superficie.size(); i++)
+	for (int i = 0; i < sensores.superficie.size(); i++)
 		cout << sensores.superficie[i];
 	cout << endl;
 
@@ -34,11 +127,12 @@ Action ComportamientoJugador::think(Sensores sensores){
 	cout << "Vida: " << sensores.vida << endl;
 	cout << endl;
 
-
 	// Determinar el efecto de la ultima accion enviada
+	last_action = accion;
 	return accion;
 }
 
-int ComportamientoJugador::interact(Action accion, int valor){
-  return false;
+int ComportamientoJugador::interact(Action accion, int valor)
+{
+	return false;
 }
--- Comportamientos_Jugador/jugador.hpp
+++ Comportamientos_Jugador/jugador.hpp
@@ -4,12 +4,22 @@
 #include "comportamientos/comportamiento.hpp"
 using namespace std;
 
+struct state {
+  int fil;
+  int col; 
+  Orientacion brujula ;
+} ;
+
 class ComportamientoJugador : public Comportamiento{
 
   public:
     ComportamientoJugador(unsigned int size) : Comportamiento(size){
       // Constructor de la clase
       // Dar el valor inicial a las variables de estado
+      current_state.fil = current_state.col = 99 ;
+      current_state.brujula = norte ;
+      last_action = actIDLE ;
+      girar_derecha=false ;
     }
 
     ComportamientoJugador(const ComportamientoJugador & comport) : Comportamiento(comport){}
@@ -19,7 +29,10 @@ class ComportamientoJugador : public Comportamiento{
     int interact(Action accion, int valor);
 
   private:
-  
+  state current_state ;
+  Action last_action ;
+  bool girar_derecha ;
+
   // Declarar aquí las variables de estado
 
 };
