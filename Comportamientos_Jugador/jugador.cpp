fa0632155f2f929cfc8e8cf662f66d444f860eaa
AlexRP <aleeexrp02@gmail.com>
2023-03-31 18:33:21 +0200

    Arreglada función de cálculo de tiempo

--- Comportamientos_Jugador/jugador.cpp
+++ Comportamientos_Jugador/jugador.cpp
@@ -147,98 +147,105 @@ int calcColIzqDer(Orientacion direccion, state estado, bool izq)
 	return sol;
 }
 
-void dibujarDiagonales(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz, int fil0, int col0, int sumaf, int sumac, bool comienzaCol) {
-	int pos = 0 ;
-	int posF0 = st.fil ;
-	int posC0 = st.col ;
-	matriz[posF0][posC0] = terreno[pos] ;
-	pos++ ;
+void dibujarDiagonales(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz, int fil0, int col0, int sumaf, int sumac, bool comienzaCol)
+{
+	int pos = 0;
+	int posF0 = st.fil;
+	int posC0 = st.col;
+	matriz[posF0][posC0] = terreno[pos];
+	pos++;
 
 	// Bucle para primera profundidad
-	int posF = posF0 + fil0 ;
-	int posC = posC0 + col0 ;
-	for (pos ; pos<=3 ; pos++) {
-		//Si está dentro de los límites escribimos
-		if (posF>=0 && posF<matriz.size() && posC>=0 && posC<matriz[0].size())
-			matriz[posF][posC] = terreno[pos] ;
+	int posF = posF0 + fil0;
+	int posC = posC0 + col0;
+	for (pos; pos <= 3; pos++)
+	{
+		// Si está dentro de los límites escribimos
+		if (posF >= 0 && posF < matriz.size() && posC >= 0 && posC < matriz[0].size())
+			matriz[posF][posC] = terreno[pos];
 
 		// Actualizamos la posición, dependiendo de si empieza a sumar por columnas o no
-		if(comienzaCol && pos < 2)
-			posC = posC + sumac ;
-		else if(!comienzaCol && pos < 2)
-			posF = posF + sumaf ;
-		else if(comienzaCol && pos >= 2)
-			posF = posF + sumaf ;
-		else if(!comienzaCol && pos >= 2)
-			posC = posC + sumac ;
-
+		if (comienzaCol && pos < 2)
+			posC = posC + sumac;
+		else if (!comienzaCol && pos < 2)
+			posF = posF + sumaf;
+		else if (comienzaCol && pos >= 2)
+			posF = posF + sumaf;
+		else if (!comienzaCol && pos >= 2)
+			posC = posC + sumac;
 	}
 
 	// Bucle para segunda profundidad
-	posF = posF0 + fil0*2 ;
-	posC = posC0 + col0*2 ;
-	for (pos ; pos<=8 ; pos++) {
-		if (posF>=0 && posF<matriz.size() && posC>=0 && posC<matriz[0].size())
-			matriz[posF][posC] = terreno[pos] ;
+	posF = posF0 + fil0 * 2;
+	posC = posC0 + col0 * 2;
+	for (pos; pos <= 8; pos++)
+	{
+		if (posF >= 0 && posF < matriz.size() && posC >= 0 && posC < matriz[0].size())
+			matriz[posF][posC] = terreno[pos];
 
-		if(comienzaCol && pos < 6)
-			posC = posC + sumac ;
-		else if(!comienzaCol && pos < 6)
-			posF = posF + sumaf ;
-		else if(comienzaCol && pos >= 6)
-			posF = posF + sumaf ; 
-		else if(!comienzaCol && pos >= 6)
-			posC = posC + sumac ;
+		if (comienzaCol && pos < 6)
+			posC = posC + sumac;
+		else if (!comienzaCol && pos < 6)
+			posF = posF + sumaf;
+		else if (comienzaCol && pos >= 6)
+			posF = posF + sumaf;
+		else if (!comienzaCol && pos >= 6)
+			posC = posC + sumac;
 	}
 
 	// Bucle para tercera profundidad
-	posF = posF0 + fil0*3 ;
-	posC = posC0 + col0*3 ;
-	for (pos ; pos<=15 ; pos++) {
-		if (posF>=0 && posF<matriz.size() && posC>=0 && posC<matriz[0].size())
-			matriz[posF][posC] = terreno[pos] ;
+	posF = posF0 + fil0 * 3;
+	posC = posC0 + col0 * 3;
+	for (pos; pos <= 15; pos++)
+	{
+		if (posF >= 0 && posF < matriz.size() && posC >= 0 && posC < matriz[0].size())
+			matriz[posF][posC] = terreno[pos];
 
-		if(comienzaCol && pos < 12)
-			posC = posC + sumac ;
-		else if(!comienzaCol && pos < 12)
-			posF = posF + sumaf ;
-		else if(comienzaCol && pos >= 12)
-			posF = posF + sumaf ; 
-		else if(!comienzaCol && pos >= 12)
-			posC = posC + sumac ;
+		if (comienzaCol && pos < 12)
+			posC = posC + sumac;
+		else if (!comienzaCol && pos < 12)
+			posF = posF + sumaf;
+		else if (comienzaCol && pos >= 12)
+			posF = posF + sumaf;
+		else if (!comienzaCol && pos >= 12)
+			posC = posC + sumac;
 	}
 }
 
-void dibujarEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz, Orientacion direccion) {
+void dibujarEnMatriz(const vector<unsigned char> &terreno, const state &st, vector<vector<unsigned char>> &matriz, Orientacion direccion)
+{
 	// Tenemos que dibujar para las 4 diagonales, noreste, sureste, suroeste y noroeste
 
 	// Noroeste
-	if(direccion == noroeste)
-		dibujarDiagonales(terreno, st, matriz, 0, -1, -1, 1, false) ;
+	if (direccion == noroeste)
+		dibujarDiagonales(terreno, st, matriz, 0, -1, -1, 1, false);
 
 	// Noreste
-	if(direccion == noreste)
-		dibujarDiagonales(terreno, st, matriz, -1, 0, 1, 1, true) ;
+	if (direccion == noreste)
+		dibujarDiagonales(terreno, st, matriz, -1, 0, 1, 1, true);
 
 	// Sureste
-	if(direccion == sureste)
-		dibujarDiagonales(terreno, st, matriz, 0, 1, 1, -1, false) ;
+	if (direccion == sureste)
+		dibujarDiagonales(terreno, st, matriz, 0, 1, 1, -1, false);
 
 	// Suroeste
-	if(direccion == suroeste)
-		dibujarDiagonales(terreno, st, matriz, 1, 0, -1, -1, true) ;
+	if (direccion == suroeste)
+		dibujarDiagonales(terreno, st, matriz, 1, 0, -1, -1, true);
 }
 
-bool giroIzq (char tipoCasilla, Sensores sensor) {
-	return (sensor.terreno[1] == tipoCasilla or (sensor.terreno[4] == tipoCasilla and esAccesible(sensor.terreno[1],sensor.superficie[1])) or (sensor.terreno[9] == tipoCasilla and esAccesible(sensor.terreno[1],sensor.superficie[1]) and esAccesible(sensor.terreno[4],sensor.superficie[4]))) ;
+bool giroIzq(char tipoCasilla, Sensores sensor)
+{
+	return (sensor.terreno[1] == tipoCasilla or (sensor.terreno[4] == tipoCasilla and esAccesible(sensor.terreno[1], sensor.superficie[1])) or (sensor.terreno[9] == tipoCasilla and esAccesible(sensor.terreno[1], sensor.superficie[1]) and esAccesible(sensor.terreno[4], sensor.superficie[4])));
 }
 
-bool giroDer (char tipoCasilla, Sensores sensor) {
-	return (sensor.terreno[3] == tipoCasilla or (sensor.terreno[8] == tipoCasilla and esAccesible(sensor.terreno[3],sensor.superficie[3])) or (sensor.terreno[15] == tipoCasilla and esAccesible(sensor.terreno[3],sensor.superficie[3]) and esAccesible(sensor.terreno[8],sensor.superficie[8]))) ;
+bool giroDer(char tipoCasilla, Sensores sensor)
+{
+	return (sensor.terreno[3] == tipoCasilla or (sensor.terreno[8] == tipoCasilla and esAccesible(sensor.terreno[3], sensor.superficie[3])) or (sensor.terreno[15] == tipoCasilla and esAccesible(sensor.terreno[3], sensor.superficie[3]) and esAccesible(sensor.terreno[8], sensor.superficie[8])));
 }
 
-bool sigoAlante (char tipoCasilla, Sensores sensor) {
-	return (sensor.terreno[2] == tipoCasilla or (sensor.terreno[6] == tipoCasilla and esAccesible(sensor.terreno[2],sensor.superficie[2])) or (sensor.terreno[12] == tipoCasilla and esAccesible(sensor.terreno[2],sensor.superficie[2]) and esAccesible(sensor.terreno[6],sensor.superficie[6]))) ;
+bool sigoAlante(char tipoCasilla, Sensores sensor)
+{
+	return (sensor.terreno[2] == tipoCasilla or (sensor.terreno[6] == tipoCasilla and esAccesible(sensor.terreno[2], sensor.superficie[2])) or (sensor.terreno[12] == tipoCasilla and esAccesible(sensor.terreno[2], sensor.superficie[2]) and esAccesible(sensor.terreno[6], sensor.superficie[6])));
 }
 
 =======
@@ -332,8 +339,8 @@ Action ComportamientoJugador::think(Sensores sensores)
 	if (sensores.reset)
 	{
 		bien_situado = false;
-		tieneBikini = false ;
-		tieneZapatilla = false ;
+		tieneBikini = false;
+		tieneZapatilla = false;
 	}
 
 	int a;
@@ -428,14 +435,16 @@ Action ComportamientoJugador::think(Sensores sensores)
 	cout << "Siguiente " << mapaTiempo[calcFilSig(current_state.brujula, current_state)][calcColSig(current_state.brujula, current_state)] << endl;
 	cout << "Derecha " << mapaTiempo[calcFillIzqDer(current_state.brujula, current_state, false)][calcColIzqDer(current_state.brujula, current_state, false)] << endl;
 
-	//Guarda si tiene el bikini
-	if(sensores.terreno[0] == 'K') {
-		tieneBikini=true ;
+	// Guarda si tiene el bikini
+	if (sensores.terreno[0] == 'K')
+	{
+		tieneBikini = true;
 	}
 
-	//Guarda si tiene zapatillas
-	if (sensores.terreno[0] == 'D') {
-		tieneZapatilla = true ;
+	// Guarda si tiene zapatillas
+	if (sensores.terreno[0] == 'D')
+	{
+		tieneZapatilla = true;
 	}
 
 	// Guarda en el array del mapa si ha pasado por G
@@ -473,11 +482,11 @@ Action ComportamientoJugador::think(Sensores sensores)
 
 		sensores.terreno[0] == 'A' ? accion = actTURN_BL : accion = actTURN_SL;
 	}
-	else if (sigoAlante('G',sensores) and !bien_situado)
+	else if (sigoAlante('G', sensores) and !bien_situado)
 	{
 		accion = actFORWARD;
 	}
-	else if (giroDer('G',sensores) and !bien_situado and last_action != actTURN_BL)
+	else if (giroDer('G', sensores) and !bien_situado and last_action != actTURN_BL)
 	{
 		sensores.terreno[0] == 'A' ? accion = actTURN_BR : accion = actTURN_SR;
 	}
@@ -486,64 +495,101 @@ Action ComportamientoJugador::think(Sensores sensores)
 		accion = actFORWARD;
 	}
 
-	//Priorizamos coger el bikini o las zapatillas
-	else if ((sigoAlante('K', sensores) && !tieneBikini) || (sigoAlante('D', sensores) && !tieneZapatilla)) {
-		accion = actFORWARD ;
-	}
-	else if ((giroDer('K',sensores) && !tieneBikini) || (giroDer('D',sensores) && !tieneZapatilla)) {
-		accion = actTURN_SR ;
-	}
-	else if ((giroIzq('K', sensores) && !tieneBikini) || (giroIzq('D', sensores) && !tieneZapatilla)) {
-		accion = actTURN_SL ;
-	}
-
-	//Priorizamos cambiar a una casilla que no sea agua, pues consume más energía si no tenemos el bikini
-	else if (!sigoAlante('A', sensores) && !tieneBikini && esAccesible(sensores.terreno[2], sensores.superficie[2])){
-		accion = actFORWARD ;
-	}
-	else if (!giroDer('A', sensores) && !tieneBikini && esAccesible(sensores.terreno[3], sensores.superficie[3])){
-		accion = actTURN_SR ;
-	}
-	else if (!giroIzq('A', sensores) && !tieneBikini && esAccesible(sensores.terreno[1], sensores.superficie[1])){
-		accion = actTURN_SL ;
-	}
-
-	//Priorizamos cambiar a una casilla que no sea bosque si no tenemos las zapatillas, pues consume más energía
-	else if (!sigoAlante('B', sensores) && !tieneBikini && esAccesible(sensores.terreno[2], sensores.superficie[2])){
-		accion = actFORWARD ;
-	}
-	else if (!giroDer('B', sensores) && !tieneBikini && esAccesible(sensores.terreno[3], sensores.superficie[3])){
-		accion = actTURN_SR ;
-	}
-	else if (!giroIzq('B', sensores) && !tieneBikini && esAccesible(sensores.terreno[1], sensores.superficie[1])){
-		accion = actTURN_SL ;
-	}
-
-	// Priorizamos ahora las casillas que no han sido visitadas y son accesibles
-	else if (last_action != actTURN_BR and esAccesible(sensores.terreno[1], sensores.superficie[1]) and mapaResultado[calcFillIzqDer(current_state.brujula, current_state, true)][calcColIzqDer(current_state.brujula, current_state, true)] == '?')
+	// Priorizamos coger el bikini o las zapatillas
+	else if ((sigoAlante('K', sensores) && !tieneBikini) || (sigoAlante('D', sensores) && !tieneZapatilla))
 	{
-		sensores.terreno[0] == 'A' ? accion = actTURN_BL : accion = actTURN_SL;
+		accion = actFORWARD;
 	}
-	else if (last_action != actTURN_BL and esAccesible(sensores.terreno[3], sensores.superficie[3]) and mapaResultado[calcFillIzqDer(current_state.brujula, current_state, false)][calcColIzqDer(current_state.brujula, current_state, false)] == '?')
+	else if ((giroDer('K', sensores) && !tieneBikini) || (giroDer('D', sensores) && !tieneZapatilla))
 	{
-		sensores.terreno[0] == 'A' ? accion = actTURN_BR : accion = actTURN_SR;
+		accion = actTURN_SR;
 	}
+	else if ((giroIzq('K', sensores) && !tieneBikini) || (giroIzq('D', sensores) && !tieneZapatilla))
+	{
+		accion = actTURN_SL;
+	}
+
+	// Priorizamos cambiar a una casilla que no sea agua, pues consume más energía si no tenemos el bikini
+	else if (!tieneBikini)
+	{
+		if ((!sigoAlante('A', sensores) or sensores.terreno[2] != 'A') && esAccesible(sensores.terreno[2], sensores.superficie[2]))
+		{
+			accion = actFORWARD;
+		}
+		else if (!giroDer('A', sensores) && !tieneBikini && esAccesible(sensores.terreno[3], sensores.superficie[3]))
+		{
+			accion = actTURN_SR;
+		}
+		else if (!giroIzq('A', sensores) && !tieneBikini && esAccesible(sensores.terreno[1], sensores.superficie[1]))
+		{
+			accion = actTURN_SL;
+		}
+		else
+		{
+			accion = actTURN_SR;
+		}
+	}
+
+	// Priorizamos cambiar a una casilla que no sea bosque si no tenemos las zapatillas, pues consume más energía
+	else if (!tieneZapatilla)
+	{
+		if ((!sigoAlante('B', sensores) or sensores.terreno[2] != 'B') && esAccesible(sensores.terreno[2], sensores.superficie[2]))
+		{
+			accion = actFORWARD;
+		}
+		else if (!giroDer('B', sensores) && !tieneZapatilla && esAccesible(sensores.terreno[3], sensores.superficie[3]))
+		{
+			accion = actTURN_SR;
+		}
+		else if (!giroIzq('B', sensores) && !tieneZapatilla && esAccesible(sensores.terreno[1], sensores.superficie[1]))
+		{
+			accion = actTURN_SL;
+		}
+		else if (esAccesible(sensores.terreno[2], sensores.superficie[2]))
+		{
+			accion = actFORWARD;
+		}
+		else
+		{
+			accion = actTURN_SR;
+		}
+	}
+
+	// // Priorizamos ahora las casillas que no han sido visitadas y son accesibles
+	// else if (last_action != actTURN_BR and esAccesible(sensores.terreno[1], sensores.superficie[1]) and mapaResultado[calcFillIzqDer(current_state.brujula, current_state, true)][calcColIzqDer(current_state.brujula, current_state, true)] == '?')
+	// {
+	// 	sensores.terreno[0] == 'A' ? accion = actTURN_BL : accion = actTURN_SL;
+	// }
+	// else if (last_action != actTURN_BL and esAccesible(sensores.terreno[3], sensores.superficie[3]) and mapaResultado[calcFillIzqDer(current_state.brujula, current_state, false)][calcColIzqDer(current_state.brujula, current_state, false)] == '?')
+	// {
+	// 	sensores.terreno[0] == 'A' ? accion = actTURN_BR : accion = actTURN_SR;
+	// }
 
 	// Comprobamos de las 3 casillas que tenemos en frente cual es la que hace más tiempo que recorrimos y giramos hacia ella
 
 	else if ((esAccesible(sensores.terreno[1], sensores.superficie[1]) or esAccesible(sensores.terreno[3], sensores.superficie[3])) and esAccesible(sensores.terreno[2], sensores.superficie[2]))
 	{
-		if (last_action != actTURN_BR and esAccesible(sensores.terreno[1], sensores.superficie[1]) and mapaTiempo[calcFillIzqDer(current_state.brujula, current_state, true)][calcColIzqDer(current_state.brujula, current_state, true)] < mapaTiempo[calcFilSig(current_state.brujula, current_state)][calcColSig(current_state.brujula, current_state)])
+		if (last_action == actTURN_BL or last_action == actTURN_BR or last_action == actTURN_SL or last_action == actTURN_SR)
 		{
-			sensores.terreno[0] == 'A' ? accion = actTURN_BL : accion = actTURN_SL;
+			accion = actFORWARD;
 		}
-		else if (last_action != actTURN_BL and esAccesible(sensores.terreno[3], sensores.superficie[3]) and mapaTiempo[calcFillIzqDer(current_state.brujula, current_state, false)][calcColIzqDer(current_state.brujula, current_state, false)] < mapaTiempo[calcFilSig(current_state.brujula, current_state)][calcColSig(current_state.brujula, current_state)])
+		else if ((last_action != actTURN_BL or last_action != actTURN_SL) and esAccesible(sensores.terreno[1], sensores.superficie[1]) and mapaTiempo[calcFillIzqDer(current_state.brujula, current_state, true)][calcColIzqDer(current_state.brujula, current_state, true)] < mapaTiempo[calcFilSig(current_state.brujula, current_state)][calcColSig(current_state.brujula, current_state)])
 		{
-			sensores.terreno[0] == 'A' ? accion = actTURN_BR : accion = actTURN_SR;
+			// sensores.terreno[0] == 'A' ? accion = actTURN_BL : 
+			accion = actTURN_SL;
+		}
+		else if ((last_action != actTURN_BR or last_action != actTURN_SR) and esAccesible(sensores.terreno[3], sensores.superficie[3]) and mapaTiempo[calcFillIzqDer(current_state.brujula, current_state, false)][calcColIzqDer(current_state.brujula, current_state, false)] < mapaTiempo[calcFilSig(current_state.brujula, current_state)][calcColSig(current_state.brujula, current_state)])
+		{
+			// sensores.terreno[0] == 'A' ? accion = actTURN_BR : 
+			accion = actTURN_SR;
+		}
+		else if (esAccesible(sensores.terreno[2], sensores.superficie[2]))
+		{
+			accion = actFORWARD;
 		}
 		else
 		{
-			accion = actFORWARD;
+			accion = actTURN_SR;
 		}
 	}
 	// Si es una casilla accesible, da un paso alante
